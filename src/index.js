3cf372bd4ae6f9267ab3a55dfd376898e180c4c9
Adam Grohs <adamgrohs@Adams-MacBook-Air-2.local>
2019-06-02 23:28:49 -0500

    fix: minor refactor to move to utils

--- src/index.js
+++ src/index.js
@@ -1,37 +1,7 @@
+const { newThisPeriod } = require('./utils')
 const { json, send } = require('micro')
 const { URL } = require('whatwg-url')
-const moment = require('moment-timezone')
 const cors = require('micro-cors')()
-const admin = require('firebase-admin')
-
-const _firebaseConfig = {
-  type: 'service_account',
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: `-----BEGIN PRIVATE KEY-----${
-    process.env.FIREBASE_PRIVATE_KEY
-  }-----END PRIVATE KEY-----\n`.replace(/\\n/g, '\n'),
-  client_email: `firebase-adminsdk-3gpvn@${
-    process.env.FIREBASE_PROJECT_ID
-  }.iam.gserviceaccount.com`,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
-  token_uri: 'https://oauth2.googleapis.com/token',
-  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-  client_x509_cert_url: `https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-3gpvn%40${
-    process.env.FIREBASE_PROJECT_ID
-  }.iam.gserviceaccount.com`
-}
-
-// console.log('_firebaseConfig',_firebaseConfig)
-if (!admin.apps.length) {
-  const admin = require('firebase-admin')
-  admin.initializeApp({
-    credential: admin.credential.cert(_firebaseConfig),
-    databaseURL: `https://${_firebaseConfig.project_id}.firebaseio.com`
-  })
-}
-const firestore = admin.firestore()
 
 const _toJSON = error => {
   return !error
@@ -135,56 +105,16 @@ module.exports = cors(async (req, res) => {
   try {
     const body = await json(req)
     // console.log('body',body)
-
-    const trackedRef = firestore
-      .collection('applications')
-      .doc(`${body.applicationId}`)
-      .collection(`${body.collectionId}`)
-      .doc(`${body.subscription.id}`)
-      .collection(
-        `${moment
-          .unix(body.subscription.current_period_start)
-          .format('MMDDYYYY')}_${moment
-          .unix(body.subscription.current_period_end)
-          .format('MMDDYYYY')}`
-      )
-      .doc(`${body.tracked.id}`)
-
-    return trackedRef
-      .get()
-      .then(billingPeriodTrackedRecord => {
-        // console.log('billingPeriodTrackedRecord',billingPeriodTrackedRecord)
-        return trackedRef
-          .set(
-            {
-              value: body.tracked.value,
-              usage: admin.firestore.FieldValue.arrayUnion(
-                moment()
-                  .utc()
-                  .format()
-              )
-            },
-            { merge: true }
-          )
-          .then(() => {
-            // console.log(
-            //   `trackedRef, newThisPeriod: ${!billingPeriodTrackedRecord.exists}`
-            // )
-            return send(res, 200, {
-              newThisPeriod: !billingPeriodTrackedRecord.exists
-            })
-          })
-          .catch(error => {
-            const jsonError = _toJSON(error)
-            return send(res, error.statusCode || 500, jsonError)
-          })
+    return newThisPeriod(body)
+      .then(isNewThisPeriod => {
+        return send(res, 200, {
+          newThisPeriod: isNewThisPeriod
+        })
       })
       .catch(error => {
         const jsonError = _toJSON(error)
         return send(res, error.statusCode || 500, jsonError)
       })
-
-    return send(res, 200, JSON.stringify({ body: 'ok' }))
   } catch (error) {
     const jsonError = _toJSON(error)
     return send(res, 500, jsonError)
